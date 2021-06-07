80ad952b0340841023cb308c4cbc4a56f51b3b18
jakravit <jakravit@Forza.ndc.nasa.gov>
2021-06-07 13:13:51 -0700

    updates

---
+++ evaluate/MLP_cross_val.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 12:07:14 2021
+
+@author: jakravit
+"""
+#from MDNregressor import MDNregressor
+from testing.MLPregressor import MLPregressor
+from sklearn.model_selection import KFold
+import evaluate.scorers as sc
+import sklearn.metrics as metrics
+
+
+def reg_prep_results(cols):
+    results = {}
+    for var in cols:
+        if var == 'cluster':
+            continue
+        results[var] = {'ytest': [],
+                        'yhat': [],
+                        'R2': [],
+                        'RMSE': [],
+                        'RMSELE': [],
+                        'Bias': [],
+                        'MAPE': [],
+                        'rRMSE': []}
+        results['fit_time'] = []
+        results['pred_time'] = []
+        results['train_loss'] = []
+        results['val_loss'] = []
+    
+    return results
+        
+
+def reg_cross_val(X,y,ti,scores):
+
+    scoreDict = {
+                'clfScore' : {'Accuracy': metrics.accuracy_score,
+                              'ROC_AUC': metrics.roc_auc_score},        
+                'regScore' : {'R2': sc.r2,
+                              'RMSE': sc.rmse,
+                              'RMSELE': sc.rmsele,
+                              'Bias': sc.bias,
+                              'MAPE': sc.mape,
+                              'rRMSE': sc.rrmse,},
+                'regLogScore' : {'R2': sc.r2,
+                                 'RMSE': sc.log_rmse,
+                                 'RMSELE': sc.log_rmsele,
+                                 'Bias': sc.log_bias,
+                                 'MAPE': sc.log_mape,
+                                 'rRMSE': sc.log_rrmse,}
+                }
+    
+    scores = scoreDict[scores]
+    results = reg_prep_results(y.columns)
+    
+    kfold = KFold(n_splits=ti['cv'], shuffle=True)
+    count = 0
+    for train, test in kfold.split(X, y):
+        
+        print ('FOLD = {}...'.format(count))
+        X_train, X_test = X.iloc[train,:], X.iloc[test,:]
+        y_train, y_test = y.iloc[train,:], y.iloc[test,:]   
+        
+        n_in = X_train.shape[1]
+        n_out = y_train.shape[1]
+        
+        model = MLPregressor(n_in,n_out,ti['epochs'],ti['batch_size'],
+                             ti['lrate'],ti['split'])
+        
+        model.build(ti['layers'])
+        
+        history = model.fit(X_train,y_train)
+        results['train_loss'].append(history.history['loss'])
+        results['val_loss'].append(history.history['val_loss'])
+        
+        y_hat = model.predict(X_test, y_test)
+        
+        final = model.evaluate(y_test,y_hat,results,scores,log=True) 
+        count = count+1
+    
+    return final
---
+++ evaluate/main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 15:22:28 2021
+
+@author: jkravz311
+"""
+#%%
+import pickle
+import pandas as pd
+import numpy as np
+# from classifier_eval import classifier
+import testing.scorers as sc
+import sklearn.metrics as metrics
+import warnings
+warnings.filterwarnings("ignore")
+from testing.MLP_cross_val import reg_cross_val
+import features as feats
+import featuretools as ft
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# data
+#rrsData = pickle.load( open( "/content/drive/My Drive/sensorIDX_rrs.p", "rb" ) )
+#rrsData = pickle.load( open( "/content/drive/MyDrive/sensorIDX_rrs.p", "rb" ) )
+rrsData = pickle.load( open( "/Users/jakravit/Desktop/npp_projects/ML/sensorIDX_rrs.p", "rb" ) )
+
+train_info = {'epochs':10,
+              'batch_size':512,
+              'lrate':1e-4,
+              'split':.2,
+              'n_mixes':5,
+              'layers':[512,128,64,32],
+              'cv':3,
+              }
+
+# get features, outputs
+atcor = 'rrs'
+sensor = 'hico'
+targets = ['chl','PC','cnap','cdom','admix','aphy440','ag440','anap440','bbphy440','bbnap440']
+X,y_tot = feats.getXY(rrsData,sensor,targets)
+targets = ['aphy440','ag440','anap440']
+X,y_a = feats.getXY(rrsData,sensor,targets)
+targets = ['aphy440']
+X,y_phy = feats.getXY(rrsData,sensor,targets)
+targets = ['ag440']
+X,y_ag = feats.getXY(rrsData,sensor,targets)
+targets = ['anap440']
+X,y_nap = feats.getXY(rrsData,sensor,targets)
+
+#%%
+runs = {}
+runs['features'] = {'X': X,
+                    'minmax': feats.minMaxScale(X, (X.min()).min(), (X.max()).max()),
+                    'standard': feats.standardScale(X),
+                    'maxabs': feats.maxAbs(X),
+                    'robust': feats.robust(X),
+                    'quantile': feats.quantile(X),
+                    'log': np.log(X),
+                    'power': feats.power(X),
+                    'norm': feats.l2norm(X)}
+runs['targets'] = {'tot': y_tot,
+                   'a': y_a,
+                   'phy': y_phy,
+                   'ag': y_ag,
+                   'nap': y_nap,
+                   'tot_log': np.log(y_tot),
+                   'a_log': np.log(y_a),
+                   'phy_log': np.log(y_phy),
+                   'ag_log': np.log(y_ag),
+                   'nap_log': np.log(y_nap)}
+
+#%% 
+results = {}
+for f in runs['features']:
+    for t in runs['targets']:
+        
+        X = runs['features'][f]
+        y = runs['targets'][t]
+        out = reg_cross_val(X, y, train_info, 'regLogScore')
+        name = '_'.join([f,t])
+        results[name] = out
+
+# save run to disk
+fname = '/content/drive/My Drive/test_results.p'
+f = open(fname,'wb')
+pickle.dump(results,f)
+f.close() 
